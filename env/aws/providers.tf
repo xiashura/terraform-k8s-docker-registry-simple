03b2c8a4d5ef6624ec18c7a9881aa09971a82b28
xiashura <plexanov@protonmail.com>
2022-10-29 00:34:22 +0300

    init aws

---
+++ env/aws/providers.tf
@@ -0,0 +1,14 @@
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.14.0"
+    }
+  }
+}
